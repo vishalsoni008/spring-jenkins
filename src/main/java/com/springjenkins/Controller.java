3b169bed0db6b8af4a7d13c761eedbde5f74497e
Vishal Soni <50075856+vishalsoni008@users.noreply.github.com>
2021-12-14 13:13:00 +0530

    second commit

--- src/main/java/com/springjenkins/Controller.java
+++ src/main/java/com/springjenkins/Controller.java
@@ -1,10 +1,14 @@
 package com.springjenkins;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+	
+	Logger log = LoggerFactory.getLogger(Controller.class);
 	
 	@GetMapping("/")
 	public String demo() {
--- src/main/java/com/springjenkins/SpringJenkinsApplication.java
+++ src/main/java/com/springjenkins/SpringJenkinsApplication.java
@@ -1,13 +1,19 @@
 package com.springjenkins;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringJenkinsApplication {
 
+	static Logger log = LoggerFactory.getLogger(Controller.class);
+	
 	public static void main(String[] args) {
+		log.info("stating spring boot app");
 		SpringApplication.run(SpringJenkinsApplication.class, args);
+		log.info("stated spring boot app");
 	}
 
 }
