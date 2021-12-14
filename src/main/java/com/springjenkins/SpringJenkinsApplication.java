91349c98e31af110425c6b9e16734bdb09908d11
Vishal Soni <50075856+vishalsoni008@users.noreply.github.com>
2021-12-14 12:18:38 +0530

    commit repository

---
+++ src/main/java/com/springjenkins/SpringJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.springjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springjenkins/SpringJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
