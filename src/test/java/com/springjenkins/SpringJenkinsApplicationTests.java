7391207329936fc3b180d7a48e1cdd2206a48636
Vishal Soni <50075856+vishalsoni008@users.noreply.github.com>
2021-12-18 15:58:08 +0530

    update one test case

--- src/test/java/com/springjenkins/SpringJenkinsApplicationTests.java
+++ src/test/java/com/springjenkins/SpringJenkinsApplicationTests.java
@@ -1,13 +1,17 @@
 package com.springjenkins;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringJenkinsApplicationTests {
+	Logger log = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
 
 	@Test
 	void contextLoads() {
+		log.info("first test case with void");
 	}
 
 }
